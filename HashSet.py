a68ce2d9a41dce085c1003b58d6805e0705fa278
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-09 22:23:26 -0400

    removed the defalut value from node

--- HashSet.py
+++ HashSet.py
@@ -55,7 +55,7 @@ class MyHashSet:
         
 
 class Node: 
-    def __init__(self, data=0, next_node=None):
+    def __init__(self, data=None, next_node=None):
         self.data = data 
         self.next_node = next_node
 
